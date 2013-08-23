35625842d52ce3f265c8a8fe67e273b2eb96f762
Mark Kusch <mark.kusch@gmail.com>
2013-08-23 17:10:30 +0200

    Prepare further puppet nodes definition

--- vagrant/puppet/manifests/nodes.pp
+++ vagrant/puppet/manifests/nodes.pp
@@ -13,12 +13,29 @@ node scdidx inherits base{
     image     => 'samalba/docker-registry',
     command   => '',
     ports     => ['5000:5000'],
+    require   => Docker::Image['samalba/docker-registry'],
   }
 
 }
 
 node scdapp inherits base{
+
+  docker::image{'silpion/jenkins':
+    registry  => 'scdidx:5000',
+    ensure    => present,
+  }
+
+  docker::run{'jenkins':
+    image     => 'silpion/jenkins',
+    command   => '/usr/local/sbin/jenkins.sh',
+    ports     => ['8080:8080', '8443:8443'],
+    require   => Docker::Image['silpion/jenkins'],
+  }
+
 }
 
 node scdadm inherits base{
+
+  include solutionscamphacks::adm
+
 }
