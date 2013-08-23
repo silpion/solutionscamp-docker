a4c4dfad5f0ec10919ad573ac31cb69da5b3a550
Mark Kusch <mark.kusch@gmail.com>
2013-08-23 12:45:56 +0200

    Pull and run samalba/docker-registry container on scdidx

--- vagrant/puppet/manifests/nodes.pp
+++ vagrant/puppet/manifests/nodes.pp
@@ -4,6 +4,17 @@ node base{
 }
 
 node scdidx inherits base{
+
+  docker::image{'samalba/docker-registry':
+    ensure    => present,
+  }
+
+  docker::run{'registry':
+    image     => 'samalba/docker-registry',
+    command   => '',
+    ports     => ['5000:5000'],
+  }
+
 }
 
 node scdapp inherits base{
