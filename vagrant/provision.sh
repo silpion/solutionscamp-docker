15513dbf6887d88f17d99f6c06b3241c1999eda0
Mark Kusch <mark.kusch@gmail.com>
2013-08-23 14:36:16 +0200

    Use a shell script for provisioning
    To guarantee a usable networking configuration in a private
    network, which feels odd to me with Vagrant/VirtualBox, use
    a shell script for provisioning, which will
    - call puppet
    - fixup networking configuration

---
+++ vagrant/provision.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+set -ex
+
+
+: ${cmd_puppet:=$( command -v puppet )}
+: ${cmd_ip:=$( command -v ip )}
+
+
+# Puppet is used for provisioning
+_provision()
+{
+  $cmd_puppet apply \
+    --modulepath /opt/puppet/modules \
+    /vagrant/puppet/manifests/site.pp
+}
+
+
+# I didn't manage to get networking running correctly.
+#   Fix networking here with some hardcoded assumptions.
+_networking()
+{
+  _ip="$( awk -v h=${HOSTNAME} '{if($0 ~ h){print $1}}' < /etc/hosts )"
+  _net='10.11.12.8'
+  _mask='29'
+  _iface='eth2'
+  $cmd_ip addr replace ${_ip}/${_mask} dev $_iface || true
+  $cmd_ip link set dev $_iface up || true
+  $cmd_ip route replace $_net dev $_iface || true
+}
+
+
+# Run provisioning first. This will provide a usable /etc/hosts file
+#   for any node in question.
+_provision
+_networking
