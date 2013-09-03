c10c92d73f27a225e46314eaee676027d15cce96
Mark Kusch <mark.kusch@gmail.com>
2013-09-03 14:03:54 +0200

    Fix puppet execution ordering issues
    garethr-docker puppet module has been updated to reflect latest
    development within dotcloud/docker.
    This fixes some order of execution issues in a deeply hackish
    way.

--- vagrant/puppet/manifests/nodes.pp
+++ vagrant/puppet/manifests/nodes.pp
@@ -1,10 +1,9 @@
-node base{
-  class { 'docker': }
+node scdreg{
+
+  class {'docker': }
     ->
   class { 'solutionscamphacks': }
-}
-
-node scdreg inherits base{
+    ->
 
   docker::image{'samalba/docker-registry':
     ensure    => present,
@@ -12,7 +11,6 @@ node scdreg inherits base{
 
   docker::run{'registry':
     image     => 'samalba/docker-registry',
-    #command   => 'gunicorn --access-logfile - --log-level debug --debug -b 0.0.0.0:80 -w 1 wsgi:application',
     command   => '',
     ports     => ['5000:5000'],
     require   => Docker::Image['samalba/docker-registry'],
@@ -20,14 +18,19 @@ node scdreg inherits base{
 
 }
 
-node scdapp inherits base{
+node scdapp{
+
+  class {'docker': }
+    ->
+  class { 'solutionscamphacks': }
+    ->
 
   docker::image{'registry.silpion.de:5000/silpion/jenkins':
     ensure    => present,
   }
 
   docker::run{'jenkins':
-    image     => 'silpion/jenkins',
+    image     => 'registry.silpion.de:5000/silpion/jenkins',
     command   => '/usr/local/sbin/jenkins.sh',
     ports     => ['8080:8080', '8443:8443'],
     require   => Docker::Image['registry.silpion.de:5000/silpion/jenkins'],
